3a0346c458344cf59c452fbc9ce0739be7e58320
thiago lopes de mello <timabuntu@gmail.com>
2022-06-11 13:12:38 -0300

    new title

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className='App'>
-      <h1 className='text-3xl'>ReactJs Ninja</h1>
+      <h1 className='text-3xl'>ReactJs Hook-useState</h1>
     </div>
   );
 }
