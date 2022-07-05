2fcc661e05ebca09ca6a77dd17344d3d54031df7
thiago lopes de mello <timabuntu@gmail.com>
2022-07-05 14:54:52 -0300

    chore: new title h2

--- src/components/useState/Lesson01.jsx
+++ src/components/useState/Lesson01.jsx
@@ -1,3 +1,7 @@
 export function Lesson01() {
-  return <h2>Lesson 01</h2>;
+  return (
+    <h2 className='font-semibold text-center uppercase'>
+      Detalhes sobre o funcionamento do useState
+    </h2>
+  );
 }
