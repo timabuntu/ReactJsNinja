bdf59377f82b40e1d38bc6dc95cf66d11b3ab447
thiago lopes de mello <timabuntu@gmail.com>
2022-07-05 15:44:32 -0300

    feat: boilerplate for useState study

--- src/components/useState/Lesson01.jsx
+++ src/components/useState/Lesson01.jsx
@@ -1,14 +1,42 @@
 import { useState } from 'react';
 
 export function Lesson01() {
-  const [valor, setValor] = useState();
+  const [valor, setValor] = useState(10);
 
+  const decremento = () => {
+    return console.log('Decrementou');
+  };
+  const incremento = () => {
+    return console.log('Incrementou');
+  };
   return (
     <>
-      <h2 className='font-semibold text-center uppercase'>
+      <h2 className='text-xl font-semibold text-center text-blue-800 uppercase'>
         Detalhes sobre o funcionamento do useState
       </h2>
-      <main className='p-8'></main>
+      <main className='p-8'>
+        <h3 className='text-xl font-semibold text-emerald-600'>
+          Incremento e Decremento
+        </h3>
+
+        <div className='flex gap-4 mt-4'>
+          <button
+            className='flex items-center justify-center p-4 bg-gray-700 rounded text-cyan-300'
+            onClick={decremento}
+          >
+            Decremento -
+          </button>
+          <button
+            className='p-4 bg-gray-700 rounded text-cyan-300'
+            onClick={incremento}
+          >
+            Incremento +
+          </button>
+        </div>
+        <p className='block p-4 mt-4 text-center text-gray-100 bg-purple-700 rounded'>
+          Valor: {valor}
+        </p>
+      </main>
     </>
   );
 }
