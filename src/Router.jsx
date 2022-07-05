d828422bdac639ffd31e083670efe1a898403b8f
thiago lopes de mello <timabuntu@gmail.com>
2022-07-05 14:43:45 -0300

    feat: added UseState router

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import { Home } from './pages/Home';
+import { UseState } from './pages/UseState';
 
 export function Router() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
+      <Route path='/usestate' element={<UseState />} />
     </Routes>
   );
 }
