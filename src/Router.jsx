6c9e2b6c232ce3e38465aa560f47ac40009e397c
thiago lopes de mello <timabuntu@gmail.com>
2022-07-11 22:42:47 -0300

    feat: created router

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
+import { Layout } from './components/Layout';
 import { Home } from './pages/Home';
 import { UseState } from './pages/UseState';
 
 export function Router() {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/usestate' element={<UseState />} />
+      <Route path='/' element={<Layout />}>
+        <Route path='/' element={<Home />} />
+        <Route path='/usestate' element={<UseState />} />
+      </Route>
     </Routes>
   );
 }
