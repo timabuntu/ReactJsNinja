7b85b271165dc85448465e7feecd4d3f4d2019ae
thiago lopes de mello <timabuntu@gmail.com>
2022-07-12 15:35:10 -0300

    style: changed size text title

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,7 +5,7 @@ export function Layout() {
   return (
     <div className='flex flex-col min-h-screen bg-neutral-800'>
       <header className='py-4 border-b border-neutral-700'>
-        <h1 className='text-2xl font-bold text-center capitalize text-neutral-300'>
+        <h1 className='text-4xl font-bold text-center capitalize text-neutral-300'>
           React Ninja
         </h1>
       </header>
