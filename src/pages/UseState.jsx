4e40fcb037cd3ee9524d57eda2bf2d556403dd38
thiago lopes de mello <timabuntu@gmail.com>
2022-07-12 15:34:17 -0300

    chore: added margin top

--- src/pages/UseState.jsx
+++ src/pages/UseState.jsx
@@ -3,7 +3,7 @@ import { Lesson01 } from '../components/useState/Lesson01';
 export function UseState() {
   return (
     <div className='w-full'>
-      <h1 className='text-3xl font-bold text-center text-neutral-300'>
+      <h1 className='mt-4 text-3xl font-bold text-center text-neutral-300'>
         · Hook - useState ·
       </h1>
       <div className='mt-8'>
