62b73d510cd01db24434d79694de456a649af05b
thiago lopes de mello <timabuntu@gmail.com>
2022-07-12 15:31:56 -0300

    style: changed elements

--- src/pages/UseState.jsx
+++ src/pages/UseState.jsx
@@ -1,13 +1,14 @@
 import { Lesson01 } from '../components/useState/Lesson01';
+
 export function UseState() {
   return (
-    <>
-      <h1 className='mt-4 text-3xl font-bold text-center'>
+    <div className='w-full'>
+      <h1 className='text-3xl font-bold text-center text-neutral-300'>
         · Hook - useState ·
       </h1>
-      <main className='p-8'>
+      <div className='mt-8'>
         <Lesson01 />
-      </main>
-    </>
+      </div>
+    </div>
   );
 }
